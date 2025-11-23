f14b860b6ab2af0098059c137836329530272bf3
Pouya Fallah <pooyafallah79@gmail.com>
2025-11-23 15:55:18 +0330

    fix bugs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: "standalone",
+    /* your other config options */
 };
 
 export default nextConfig;
